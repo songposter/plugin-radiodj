22ad811f437849687b10403ce3026a71fbf214c6
Benedikt Bauer <benedikt@xenzilla.de>
2016-03-31 00:11:02 +0200

    Implement Logging,  fix parameter order (scopeId before userId) and set default scopeId to 0

--- Plugin_SongPoster/WebRequester.cs
+++ Plugin_SongPoster/WebRequester.cs
@@ -1,8 +1,8 @@
 ﻿using rdjInterface;
 using System;
 using System.Net;
-using System.Threading;
-using System.Web;
+using System.Reflection;
+using System.IO;
 
 
 namespace Plugin_SongPoster
@@ -11,13 +11,15 @@ namespace Plugin_SongPoster
     {
         public void sendRequest(SongData nowPlaying, string template, string[] networks, string userId, string password)
         {
+            string filename = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\Plugin_SongPoster.log";
+
             // Parse variable message template and fill in data from nowPlaying
             string sendTextVars = Variables.Update(template, nowPlaying, false);
 
             // For each selected network, build custom URL and start a "send" task
             foreach (string network in networks)
             {
-                string sendText = "http://songposter.net/send-post/" + network + "/" + userId + "/1/" + password + "/" + HttpUtility.UrlEncode(sendTextVars);
+                string sendText = "http://songposter.net/send-post/" + network + "/0/" + userId + "/" + password + "/" + Uri.EscapeDataString(sendTextVars);
                 try
                 {
                     WebClient client = new WebClient();
@@ -25,7 +27,11 @@ namespace Plugin_SongPoster
                     {
                         if (!e.Cancelled && e.Error == null)
                         {
-                            Utils.LogMe((string)e.Result, false);
+                            using (StreamWriter outputFile = new StreamWriter(filename, true))
+                            {
+                                outputFile.WriteLine(sendText);
+                                outputFile.WriteLine((string) e.Result);
+                            }
                         }
                     };
                     client.DownloadStringAsync(new Uri(sendText));
